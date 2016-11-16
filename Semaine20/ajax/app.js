e09a9ca24a6f57f68567fbf975cfc73c1e9d52d5
myrNG <my.ni.jp@gmail.com>
2016-11-16 19:18:36 +0100

    Ajout bouton Cacher

--- Semaine20/ajax/app.js
+++ Semaine20/ajax/app.js
@@ -10,7 +10,7 @@ function loadJSON() {
 };
 // Fonctio qui charge et affiche les infos utilisateur
 function onJSON(e) {
-            console.log('onDataText', this.responseText); // this.responseText est défini
+            console.log('Données JSON', this.responseText); // this.responseText est défini
             // "apercu" est la div dans laquelle apparaitra les infos
             var apercu = document.getElementById('apercu');
             apercu.style.display = 'block';
@@ -20,22 +20,32 @@ function onJSON(e) {
             var infoList = document.createElement('div');
             // Pour chaque Objet dans "infos" on crée un paragrape contenant les données
             infos.forEach(function (info) {
+                //apercu.innerHTML = '<p>' + info.nom + ' /' + info.prenom + ' / ' + info.email + ' / ' + info.ville + '</p>';
+
                 var infoPara = document.createElement('p');
                 infoPara.innerText = info.nom + ' / ' + info.prenom + ' / ' + info.email + ' / ' + info.ville;
                 // Ces données contenues dans "infoPara" seront les enfants de "infoList"
                 infoList.appendChild(infoPara);
+
             });
             // "infoList" qui sera lui même enfant de la div "apercu"
             apercu.appendChild(infoList);
-        //FIXME Si données déjà affichées, ne pas les réafficher
+            if (requete.readyState === 4) {
+                console.log('requete statut :', requete.readyState);
+            };
 
         };
 // Fonction qui cache les infos utilisateurs
 function hideJSON(e) {
         var apercu = document.getElementById('apercu');
         apercu.style.display = 'none';
+        //FIXME Si données déjà affichées, ne pas les réafficher
+        // TODO vider le cache
 };
 
+function addJSON(e) {
+
+};
 function onError(e) {
                 alert("Ne fonctionnera qu'avec un serveur :) ", e);
 };
