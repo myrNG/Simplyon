c2ea1604ed1df8cebc51b6a90f522392ba6d46e4
myrNG <my.ni.jp@gmail.com>
2016-11-17 23:25:09 +0100

    Fonctionnalité hideJSON() ok

--- Semaine20/ajax/app.js
+++ Semaine20/ajax/app.js
@@ -1,4 +1,6 @@
 var requete;
+var apercu = document.getElementById('apercu');
+
 // Requete Ajax
 function loadJSON() {
             requete = new XMLHttpRequest();
@@ -8,11 +10,10 @@ function loadJSON() {
             requete.open('get', 'infos.json', true);
             requete.send();
 };
-// Fonctio qui charge et affiche les infos utilisateur
+// Fonction qui charge et affiche les infos utilisateur
 function onJSON(e) {
             console.log('Données JSON', this.responseText); // this.responseText est défini
             // "apercu" est la div dans laquelle apparaitra les infos
-            var apercu = document.getElementById('apercu');
             apercu.style.display = 'block';
             // "infos" stock les données JSON
             var infos = JSON.parse(this.responseText)['infos'];
@@ -20,27 +21,24 @@ function onJSON(e) {
             var infoList = document.createElement('div');
             // Pour chaque Objet dans "infos" on crée un paragrape contenant les données
             infos.forEach(function (info) {
-                //apercu.innerHTML = '<p>' + info.nom + ' /' + info.prenom + ' / ' + info.email + ' / ' + info.ville + '</p>';
-
                 var infoPara = document.createElement('p');
                 infoPara.innerText = info.nom + ' / ' + info.prenom + ' / ' + info.email + ' / ' + info.ville;
                 // Ces données contenues dans "infoPara" seront les enfants de "infoList"
                 infoList.appendChild(infoPara);
-
             });
             // "infoList" qui sera lui même enfant de la div "apercu"
             apercu.appendChild(infoList);
             if (requete.readyState === 4) {
                 console.log('requete statut :', requete.readyState);
             };
-
+            //FIXME détruire les données pour réinitialiser
         };
 // Fonction qui cache les infos utilisateurs
 function hideJSON(e) {
-        var apercu = document.getElementById('apercu');
-        apercu.style.display = 'none';
-        //FIXME Si données déjà affichées, ne pas les réafficher
-        // TODO vider le cache
+        while(apercu.firstChild) {
+            apercu.removeChild(apercu.firstChild);
+        }
+
 };
 
 function addJSON(e) {
